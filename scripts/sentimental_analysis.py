8b90feece6f58718cc2041b77bcab9466592dacd
Eric Haggar <ehagg014@uottawa.ca>
2019-02-10 02:50:32 -0500

    Initialized sentimental_analysis script

---
+++ scripts/sentimental_analysis.py
@@ -0,0 +1,16 @@
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+def get_analysis(text):
+    client = language.LanguageServiceClient()
+
+    document = types.Document(
+        content = text,
+        type = enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+    return sentiment.score, sentiment.magnitude
+    
