38b3fb6f4dbf9feed94bc606f25f753aea8f39fb
Eric Haggar <ehagg014@uottawa.ca>
2019-02-10 09:22:39 -0500

    Included Firebase in master script

--- scripts/master.py
+++ scripts/master.py
@@ -1,6 +1,19 @@
 import twitter
 import sentimental_analysis
 import keyword_detection
+import pyrebase
+import config
+
+configuration = {
+    "apiKey": config.api_key,
+    "authDomain": config.auth_domain,
+    "databaseURL": config.database_URL,
+    "projectId": config.project_id,
+    "storageBucket": config.storage_bucket,
+} 
+
+firebase = pyrebase.initialize_app(configuration)
+database = firebase.database()
 
 sentiment_scores = list()
 category_scores = dict()
@@ -14,12 +27,19 @@ tweets = twitter.get_tweets(username)
 if (tweets == -1):
     print("The user does not exist!")
 else: 
+    database.child(username).child("sentiment_scores").remove()
+    database.child(username).child("category_scores").remove()
+
     for tweet in tweets:
         sentiment_score = sentimental_analysis.get_scores(tweet)
         sentiment_scores.append(sentiment_score)
 
-
     category_scores = keyword_detection.calculate_scores(tweets)
+
+    database.child(username).child("sentiment_scores").set(sentiment_scores)
+    database.child(username).child("category_scores").set(category_scores)
+
+    
     
 
 
