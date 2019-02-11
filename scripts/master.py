da70107d7b3902d1c67f52157cbde051ca9b97c6
Eric Haggar <ehagg014@uottawa.ca>
2019-02-10 19:24:40 -0500

    Completed sentiment analysis plot script

--- scripts/master.py
+++ scripts/master.py
@@ -3,50 +3,47 @@ import sentimental_analysis
 import keyword_detection
 import pyrebase
 import config
-import sys
+import plot
+
+sentiment_scores = list()
+category_scores = dict()
+
+file = open("username.txt", "r")
+username = file.readline()
+
+configuration = {
+
+    "apiKey": config.api_key,
+    "authDomain": config.auth_domain,
+    "databaseURL": config.database_URL,
+    "projectId": config.project_id,
+    "storageBucket": config.storage_bucket,
+
+    }
+
+firebase = pyrebase.initialize_app(configuration)
+database = firebase.database()
+
+# gets user tweets
+tweets = twitter.get_tweets(username)
+
+if (tweets == -1):
+    print("The user does not exist!")
+else:
+    database.child(username).child("sentiment_scores").remove()
+    database.child(username).child("category_scores").remove()
+
+for tweet in tweets:
+    sentiment_score = sentimental_analysis.get_scores(tweet)
+    sentiment_scores.append(sentiment_score)
+
+category_scores = keyword_detection.calculate_scores(tweets)
+
+database.child(username).child("sentiment_scores").set(sentiment_scores)
+database.child(username).child("category_scores").set(category_scores)
+
+plot.generate_plots(sentiment_scores, category_scores)
 
 
-def masterScript():
-    sentiment_scores = list()
-    category_scores = dict()
 
-    file = open("username.txt", "r")
-    username = file.readline()
-
-    configuration = {
-
-        "apiKey": config.api_key,
-        "authDomain": config.auth_domain,
-        "databaseURL": config.database_URL,
-        "projectId": config.project_id,
-        "storageBucket": config.storage_bucket,
-    } 
-
-    firebase = pyrebase.initialize_app(configuration)
-    database = firebase.database()
-
-    # gets user tweets
-    tweets = twitter.get_tweets(username)
-
-    if (tweets == -1):
-        print("The user does not exist!")
-    else: 
-        database.child(username).child("sentiment_scores").remove()
-        database.child(username).child("category_scores").remove()
-
-        for tweet in tweets:
-            sentiment_score = sentimental_analysis.get_scores(tweet)
-            sentiment_scores.append(sentiment_score)
-
-        category_scores = keyword_detection.calculate_scores(tweets)
-
-        database.child(username).child("sentiment_scores").set(sentiment_scores)
-        database.child(username).child("category_scores").set(category_scores)
     
-    sys.stdout.flush()
-
-masterScript()
-
-
-            
-                
---
+++ scripts/plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plot
+
+def generate_plots(sentiment_scores, category_scores):
+   
+    x_axis = list()
+    y_axis = sentiment_scores
+
+    count = 0
+
+    for i in range(len(sentiment_scores)):
+        x_axis.append(count)
+        count += i
+
+    plot.plot(x_axis,y_axis)
+
+
+    plot.xlabel('Number of Posts')
+    plot.ylabel('Sentimental Score')
+
+    plot.title('Sentiment Analysis')
+    plot.axhline(y = -1.0, linestyle  = '--', color='red')
+    plot.axhline(y = -0.25, linestyle  = '--', color='orange')
+    plot.axhline(y = 1.0, linestyle  = '--', color='green')
+    plot.show()
+
+   
+
+
+
+
+
