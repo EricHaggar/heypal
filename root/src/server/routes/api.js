b26c434402222b416d860e670de7ac2b9e44c208
Eric Haggar <ehagg014@uottawa.ca>
2019-09-05 18:50:26 -0400

    Finalized getSentimentScores route

--- root/src/server/routes/api.js
+++ root/src/server/routes/api.js
@@ -2,15 +2,24 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 var config = require('../config.js');
-var T = new Twitter(config);
+var Sentiment = require('sentiment');
+
+var twitter = new Twitter(config);
+var sentiment = new Sentiment();
+
 
 
 router.get('/getSentimentScores', (req, res) => {
-   console.log(req.body.username)
-   T.get('statuses/user_timeline.json', {screen_name: req.body.username, count: 10, lang: 'en'}, async function(error, tweets, response) {
+   twitter.get('statuses/user_timeline.json', {screen_name: req.body.username, count: 30, lang: 'en'}, async function(error, tweets, response) {
       if (!error) {
          tweetsText = await tweets.map(tweet => {return tweet.text})
-         res.send(tweetsText)
+
+         var scores = []
+         for (var i = 0; i < tweetsText.length; i++) {
+            var result = await sentiment.analyze(tweetsText[i], {language: 'en'});
+            scores.push(result.comparative)
+         }
+         res.send(scores)
       } else {
           res.send(error)
       }
