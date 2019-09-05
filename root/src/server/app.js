e727d348a7572d49a91f09875e77d417e8d52757
Eric Haggar <ehagg014@uottawa.ca>
2019-09-05 16:40:41 -0400

    Tested initial part of getSentimentScores route using Postman

--- root/src/server/app.js
+++ root/src/server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+var bodyParser = require('body-parser');
 
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', indexRouter);
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
--- root/src/server/routes/api.js
+++ root/src/server/routes/api.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var twitter = require('../twitter');
+var Twitter = require('twitter');
+var config = require('../config.js');
+var T = new Twitter(config);
 
 
 router.get('/getSentimentScores', (req, res) => {
-    res.send(twitter.getTweets("realDonaldTrump"))
+   console.log(req.body.username)
+   T.get('statuses/user_timeline.json', {screen_name: req.body.username, count: 10, lang: 'en'}, async function(error, tweets, response) {
+      if (!error) {
+         tweetsText = await tweets.map(tweet => {return tweet.text})
+         res.send(tweetsText)
+      } else {
+          res.send(error)
+      }
+  });
 });
+
+
 
 module.exports = router;
