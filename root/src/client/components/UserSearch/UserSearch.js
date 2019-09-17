7169ae0d055c34cc57b900563e8a4ba9a7c70d0c
Eric Haggar <ehagg014@uottawa.ca>
2019-09-17 00:27:15 -0400

    Modified input error behaviour

--- root/src/client/components/UserSearch/UserSearch.js
+++ root/src/client/components/UserSearch/UserSearch.js
@@ -61,7 +61,8 @@ class UserSearch extends React.Component {
                 />
                 {this.state.errors || this.state.score == undefined || this.state.scores.length < minNumberOfTweets ? this.displayMessage() : null}
             </div>
-            {this.state.validAccount && (this.state.scores != undefined) && (this.state.scores.length >= minNumberOfTweets) ? <Graph username={this.state.username} scores={this.state.scores} /> : null}
+            {this.state.validAccount && (this.state.scores != undefined) && (this.state.scores.length >= minNumberOfTweets) ? 
+            <Graph username={this.state.username} scores={this.state.scores} /> : <Graph username={"Example"} scores={[0.07142857142857142,1,0,0.4,-0.2857142857142857,0,0.125,-0.18181818181818182,0.5625,0.11764705882352941,-0.3333333333333333,0.375,0,0.15384615384615385,-0.11764705882352941,0,-0.15384615384615385,0,0.2631578947368421,0.23076923076923078]} />}
             </div>
         )
     }
--- root/src/client/pages/Home/Home.js
+++ root/src/client/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './Home.css';
 import UserSearch from '../../components/UserSearch';
+import githubIcon from '../../images/githubIcon.svg';
+import reactNode from '../../images/reactNode.png';
 
 
 class Home extends React.Component {
@@ -23,8 +25,8 @@ class Home extends React.Component {
               analysis to detect signs of depression. This tool was designed as a prototype to provide
               analytics to social platforms in order to notify close friends, family and followers to check-up
               on the user.<br /><br />
-              <h4>Technology Stack: <img src="https://hackernoon.com/hn-images/1*-NOQtyJAGQ1RNC3iVt_thA.png" alt="image" height="45" /></h4> 
-              <h4>Github Repo: <a href="https://github.com/EricHaggar/HeyPal" target="_blank"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" height="45" alt="image"/></a></h4>
+              <h4>Technology Stack: <img src={reactNode} alt="image" height="45" /></h4>
+              <h4>Github Repo: <a href="https://github.com/EricHaggar/HeyPal" target="_blank"><img src={githubIcon} height="45" alt="image" /></a></h4>
             </div>
           </div>
           <div className='search-graph-container'>
@@ -34,9 +36,9 @@ class Home extends React.Component {
             <div className='search-graph'>
               < UserSearch />
             </div>
-          </div >
+          </div>
         </div>
-      </div >
+      </div>
     )
   }
 }
